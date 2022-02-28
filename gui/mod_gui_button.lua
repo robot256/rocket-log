b8557fe21ed36d063f671b5d41813418d40fe1b0
robot256 <robot256@GMAIL.COM>
2022-02-27 21:36:21 -0500

    Add setting to control mod gui button

--- gui/mod_gui_button.lua
+++ gui/mod_gui_button.lua
@@ -7,9 +7,10 @@ local UNLOCK_TECH_NAME = "se-rocket-launch-pad"
 local function add_mod_gui_button(player)
   local flow = mod_gui.get_button_flow(player)
   
-  -- Player only gets button if they have reseached cargo rockets
+  -- Player only gets button if they have reseached cargo rockets AND have the setting enabled
   if (not player.force.technologies[UNLOCK_TECH_NAME]) or 
-     (not player.force.technologies[UNLOCK_TECH_NAME].researched) then
+     (not player.force.technologies[UNLOCK_TECH_NAME].researched) or
+     (not player.mod_settings["rocket-log-mod-button"] or not player.mod_settings["rocket-log-mod-button"].value) then
     local index = nil
     for _,n in pairs(flow.children_names) do
       if n == "rocket_log" then
@@ -43,13 +44,13 @@ local function add_mod_gui_button(player)
 end
 
 -- Check to add button when new players join
-events.on_player_joined_game(function(event)
+events.on_player_joined_game( function(event)
   local player = game.players[event.player_index]
   add_mod_gui_button(player)
 end)
 
 -- Check to add button when technology researched
-events.on_research_finished (function(event)
+events.on_research_finished( function(event)
   if event.research.name == UNLOCK_TECH_NAME then
     for _, player in pairs(event.research.force.players) do
       add_mod_gui_button(player)
@@ -58,14 +59,20 @@ events.on_research_finished (function(event)
 end)
 
 -- Check to remove button when technology unresearched
-events.on_research_reversed (function(event)
+events.on_research_reversed( function(event)
   if event.research.name == UNLOCK_TECH_NAME then
     for _, player in pairs(event.research.force.players) do
       add_mod_gui_button(player)
     end
   end
 end)
-  
+
+-- Check to add or remove button when player changes setting
+events.on_runtime_mod_setting_changed( function(event)
+  if event.setting == "rocket-log-mod-button" and event.player_index and game.players[event.player_index] then
+    add_mod_gui_button(game.players[event.player_index])
+  end
+end)
 
 return {
     add_mod_gui_button = add_mod_gui_button
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend{
+  {
+    type = "bool-setting",
+    name = "rocket-log-mod-button",
+    setting_type = "runtime-per-user",
+    default_value = true,
+    order = "a"
+  },
+}
